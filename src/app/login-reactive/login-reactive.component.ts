f348ff2e8c58bbe862c8a7eb873ab8d453e43852
Brandon Ritchie <britchie@avi>
2022-05-04 15:56:25 -0400

    Comparing Reactive and Template Driven Forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -34,4 +34,12 @@ export class LoginReactiveComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
+
+  get email() {
+    return this.form.controls["email"];
+  }
+
+  get password() {
+    return this.form.controls["password"];
+  }
 }
